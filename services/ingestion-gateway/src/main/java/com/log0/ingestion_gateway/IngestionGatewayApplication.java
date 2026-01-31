b758412f12b9e8a16c480b51cb88f6b43eda4810
Ashmit JaiSarita Gupta <ashmitgupta.social@gmail.com>
2026-01-31 12:35:16 +0530

    feat: [ingestion-gateway] initialize spring project

---
+++ services/ingestion-gateway/src/main/java/com/log0/ingestion_gateway/IngestionGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.log0.ingestion_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestionGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestionGatewayApplication.class, args);
+	}
+
+}
---
+++ services/ingestion-gateway/src/test/java/com/log0/ingestion_gateway/IngestionGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.log0.ingestion_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestionGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
