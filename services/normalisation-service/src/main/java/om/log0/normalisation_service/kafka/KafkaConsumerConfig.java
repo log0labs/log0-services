6bb097cec90f1e214ccaea51cf7c4a421671b024
Ashmit JaiSarita Gupta <ashmitgupta.social@gmail.com>
2026-02-14 02:28:08 +0530

    feat: [normalisation-service] kafka consumer config

---
+++ services/normalisation-service/src/main/java/om/log0/normalisation_service/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,23 @@
+package om.log0.normalisation_service.kafka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
+            ConsumerFactory<String, String> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(
+                ContainerProperties.AckMode.MANUAL
+        );
+
+        return factory;
+    }
+}
