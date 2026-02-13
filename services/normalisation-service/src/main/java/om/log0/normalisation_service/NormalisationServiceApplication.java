a4a91cd29017b1d1684032bef41ab16b35952ccb
Ashmit JaiSarita Gupta <ashmitgupta.social@gmail.com>
2026-02-14 01:34:48 +0530

    feat: [normalisation-service] setup

---
+++ services/normalisation-service/src/main/java/om/log0/normalisation_service/NormalisationServiceApplication.java
@@ -0,0 +1,13 @@
+package om.log0.normalisation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormalisationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NormalisationServiceApplication.class, args);
+	}
+
+}
