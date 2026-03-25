b61dc60895f50580df2a12fd960d6faff8bb363d
Ashmit JaiSarita Gupta <ashmitgupta.social@gmail.com>
2026-03-25 23:00:21 +0530

    feat: [clustering-service] scaffold Spring Boot project with Kafka, Lombok, and Jackson dependencies

---
+++ services/clustering-service/src/main/java/com/log0/clustering_service/ClusteringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.log0.clustering_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusteringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClusteringServiceApplication.class, args);
+	}
+
+}
---
+++ services/clustering-service/src/test/java/com/log0/clustering_service/ClusteringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.log0.clustering_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
